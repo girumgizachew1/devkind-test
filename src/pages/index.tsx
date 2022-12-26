e4c064b982aab84993f86a3cb4e26d88a5f94ffe
unknown <giri@10000>
2022-12-26 23:26:17 +0300

    Ignore node_modules directory

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@/layouts/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Next.js Checkout"
+          description="Donec rutrum congue leo eget malesuada. Vivamus suscipit tortor eget felis porttitor volutpat. Proin eget tortor risus. Nulla porttitor accumsan tincidunt."
+        />
+      }
+    >
+      
+    </Main>
+  );
+};
+
+export default Index;
